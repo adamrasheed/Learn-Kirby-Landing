6064d461e7d6c3a8d18a778d3684efaf565f4f38
Adam Rasheed <info@creatix.io>
2016-09-07 10:11:21 -0700

    added files

---
+++ assets/javascript.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+    $("#CTA").click(function() {
+        $(".form").slideDown(250).css('display','flex');
+        
+        // if form is visible
+        if ($(".form").is(":visible")) {
+            
+            // change .cta cursor to default + Remove Gradient
+            $("#CTA").css('cursor', 'default').css('background','hsla(28, 100%, 50%, 1)');
+            
+            // darken background color
+            $('#optin-bg').css('background', 'hsla(0, 0%, 65%, 1)');
+        }
+        $(".form__email").focus();
+	 });
+    
+    $(".nav__button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#optin-bg").offset().top
+    }, 750);
+});
+});
