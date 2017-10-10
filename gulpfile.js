b7804f5b578f8992abf220adef5e991d48262944
Adam Rasheed <adamrasheed91@gmail.com>
2017-10-10 14:16:43 -0700

    added deps

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,15 @@ cache = require('gulp-cache');
 var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var ghPages = require('gulp-gh-pages');
 
-gulp.task('browser-sync', function() {
+
+const build = {
+  styles: 'build/styles',
+  scripts: 'build/scripts'
+}
+
+gulp.task('browser-sync', () =>  {
 browserSync({
 server: {
    baseDir: "./"
@@ -23,13 +30,24 @@ gulp.task('bs-reload', function () {
 browserSync.reload();
 });
 
-gulp.task('images', function(){
+gulp.task('images', () => {
 gulp.src('app/images/**/*')
-.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-.pipe(gulp.dest('build/images/'));
+  .pipe(cache(
+    imagemin([
+      imagemin.svgo({
+        plugins: [{removeViewBox: true}]
+      }),
+      {
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+      }
+    ])
+  ))
+  .pipe(gulp.dest('build/images/'));
 });
 
-gulp.task('styles', function(){
+gulp.task('styles', () => {
 gulp.src(['app/styles/**/*.scss'])
 .pipe(plumber({
   errorHandler: function (error) {
@@ -45,24 +63,30 @@ gulp.src(['app/styles/**/*.scss'])
 .pipe(browserSync.reload({stream:true}))
 });
 
-gulp.task('scripts', function(){
+gulp.task('scripts', () => {
 return gulp.src('app/scripts/**/*.js')
-.pipe(plumber({
-  errorHandler: function (error) {
-    console.log(error.message);
-    this.emit('end');
-}}))
-.pipe(concat('main.js'))
-.pipe(babel())
-.pipe(gulp.dest('build/scripts/'))
-.pipe(rename({suffix: '.min'}))
-.pipe(uglify())
-.pipe(gulp.dest('build/scripts/'))
-.pipe(browserSync.reload({stream:true}))
+  .pipe(plumber({
+    errorHandler: function (error) {
+      console.log(error.message);
+      this.emit('end');
+  }}))
+  .pipe(concat('main.js'))
+  .pipe(babel())
+  .pipe(gulp.dest('build/scripts/'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(uglify())
+  .pipe(gulp.dest('build/scripts/'))
+  .pipe(browserSync.reload({stream:true}))
 });
 
-gulp.task('default', ['browser-sync'], function(){
+gulp.task('default', ['browser-sync'], () => {
 gulp.watch("app/styles/**/*.scss", ['styles']);
 gulp.watch("app/scripts/**/*.js", ['scripts']);
 gulp.watch("*.html", ['bs-reload']);
+});
+
+
+gulp.task('deploy', ['default'], () => {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages());
 });
