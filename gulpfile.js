e76d51388339262654202c5a29334e4f3cce1724
Adam Rasheed <adamrasheed91@gmail.com>
2017-10-10 16:30:21 -0700

    getting gulp to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,66 @@
 var gulp = require('gulp'),
 plumber = require('gulp-plumber'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
+
 var autoprefixer = require('gulp-autoprefixer');
 var babel = require('gulp-babel');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin'),
 cache = require('gulp-cache');
 var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var ghPages = require('gulp-gh-pages');
 
+var awspublish = require('gulp-awspublish');
 
-const build = {
-  styles: 'build/styles',
-  scripts: 'build/scripts'
+const app = {
+  root: 'app',
+  html: 'app/*.html',
+  styles: 'app/styles/**/*.scss',
+  scripts: 'app/scripts/**/*.js',
+  assets: 'app/assets/**/*'
 }
 
+const tmp = {
+  root: 'tmp',
+  html: 'tmp/index.html',
+  styles: 'tmp/styles/*.css',
+  scripts: 'tmp/scripts/*.js',
+  assets: 'tmp/assets'
+}
+
+const dist = {
+  root: 'dist',
+  html: 'dist/*.html',
+  styles: 'dist/styles',
+  scripts: 'dist/scripts',
+  assets: 'dist/assets/'
+}
+
+ // Browser Sync
 gulp.task('browser-sync', () =>  {
-browserSync({
-server: {
-   baseDir: "./"
-}
-});
+  browserSync({
+    server: {
+      baseDir: tmp.root
+    }
+  });
 });
 
 gulp.task('bs-reload', function () {
 browserSync.reload();
 });
 
-gulp.task('images', () => {
-gulp.src('app/images/**/*')
+
+// HTML
+gulp.task('html', () => {
+  return gulp.src(app.html)
+    .pipe(gulp.dest(tmp.html));
+});
+
+// Images
+gulp.task('assets', () => {
+gulp.src(app.assets)
   .pipe(cache(
     imagemin([
       imagemin.svgo({
@@ -44,11 +73,13 @@ gulp.src('app/images/**/*')
       }
     ])
   ))
-  .pipe(gulp.dest('build/images/'));
+  .pipe(gulp.dest(tmp.assets));
 });
 
+
+// SASS
 gulp.task('styles', () => {
-gulp.src(['app/styles/**/*.scss'])
+gulp.src([app.styles])
 .pipe(plumber({
   errorHandler: function (error) {
     console.log(error.message);
@@ -56,37 +87,52 @@ gulp.src(['app/styles/**/*.scss'])
 }}))
 .pipe(sass())
 .pipe(autoprefixer('last 2 versions'))
-.pipe(gulp.dest('build/styles/'))
+.pipe(gulp.dest(tmp.styles))
 .pipe(rename({suffix: '.min'}))
 .pipe(minifycss())
-.pipe(gulp.dest('build/styles/'))
+.pipe(gulp.dest(tmp.styles))
 .pipe(browserSync.reload({stream:true}))
 });
 
+
+// SCRIPTS
 gulp.task('scripts', () => {
-return gulp.src('app/scripts/**/*.js')
+return gulp.src(app.scripts)
   .pipe(plumber({
     errorHandler: function (error) {
       console.log(error.message);
       this.emit('end');
   }}))
-  .pipe(concat('main.js'))
   .pipe(babel())
-  .pipe(gulp.dest('build/scripts/'))
+  .pipe(gulp.dest(tmp.scripts))
   .pipe(rename({suffix: '.min'}))
   .pipe(uglify())
-  .pipe(gulp.dest('build/scripts/'))
+  .pipe(gulp.dest(tmp.scripts))
   .pipe(browserSync.reload({stream:true}))
 });
 
-gulp.task('default', ['browser-sync'], () => {
-gulp.watch("app/styles/**/*.scss", ['styles']);
-gulp.watch("app/scripts/**/*.js", ['scripts']);
-gulp.watch("*.html", ['bs-reload']);
+gulp.task('clean', () => {
+  return del([tmp.root]);
+});
+
+gulp.task('build', ['clean', 'html', 'styles', 'scripts', 'assets'], () => {
+  gulp.src(app.root)
+  .pipe(gulp.dest(tmp.root))
+
+});
+
+gulp.task('default', [ 'build', 'browser-sync'], () => {
+gulp.watch(app.styles, ['styles']);
+gulp.watch(app.scripts, ['scripts']);
+gulp.watch(app.html, ['bs-reload']);
 });
 
 
-gulp.task('deploy', ['default'], () => {
-  return gulp.src('./build/**/*')
-    .pipe(ghPages());
+
+
+
+
+gulp.task('deploy',  () => {
+  return gulp.src(app.root)
+    .pip(gulp.dest(dist.root));
 });
