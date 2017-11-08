712b6b69db9a7f50e9297f85c6871a4bd943d2dd
Adam Rasheed <adamrasheed91@gmail.com>
2017-11-08 14:14:05 -0800

    tried to improve api post

--- app/js/modules/form.js
+++ app/js/modules/form.js
@@ -1,67 +1,71 @@
 import { validate } from 'validate.js'
 import keys from './keys'
 
+
+const fetchOptions = {
+    'method': 'POST',
+    'Content-Type': 'application/json; charset=utf-8',
+};
+
 const postRequest = new Request(
-    `${keys.url}/courses/${keys.courseId}/subscriptions?api_secret=${keys.secret}`,
-    {
-        'method': 'GET',
-        'Content-Type': 'application/json'
-    },
-    {
-        body: JSON.stringify({
-            email: ''
-        })
-    }
+    `${keys.url}/forms/${keys.formId}/subscribe`,
+    fetchOptions
 );
 
-const myRequest = new Request(
-    `${keys.url}/sequences?api_key=${keys.api}`,
-    {
-        'method': 'GET',
-        'Content-Type': 'application/json'
-    }
-);
-
-
-const form = () => {
-
-    const forms = Array.from(document.querySelectorAll('form'));
-
-
-    const formFunc = forms.map( singleForm => {
-        let email = singleForm.querySelector('.form__input');
-            singleForm.addEventListener('submit', (e) => {
-                e.preventDefault();
-                // return false;
-                console.log(e.target.value)
-            });
-
-            email.addEventListener('blur', (e)=>{
-                let val = e.target.value;
-                if(val != "") {
-                    e.target.classList.add('has-value');
-                } else {
-                    e.target.classList.remove('has-value');
-                }
-            });
-        }
-    );
-
-
+const Ajax = (passedData) => {
     fetch(postRequest)
         .then(
         resp => resp.json()
         )
         .then(
         data => {
-            let crashCourse = data.courses.filter(
-                course => (course.id == keys.courseId)
+            // let crashCourse = data.courses.filter(
+            //     course => (course.id == keys.courseId)
+            // )
+            // console.log(...crashCourse);
+
+            let form = data.forms.filter(
+                form => (form.id == keys.formId)
             )
-            console.log(...crashCourse);
-            console.log('hi');
+            console.log(form);
+            let messageSuccess = form.success_message;
         }
         )
-        .catch(error => console.log(`Error: ${error.message}`));
+        .catch(error => {
+            console.log(error.message);
+
+        });
+}
+
+
+const form = () => {
+
+    const forms = Array.from(document.querySelectorAll('form'));
+
+    const formFunc = forms.map( singleForm => {
+        let email = singleForm.querySelector('.form__input');
+        let formMsg = singleForm.querySelector('.form__msg-container');
+        let formSuccess = formMsg.querySelector('.form__msg--success');
+        
+        singleForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let inputEmail = e.target.querySelector('.form__input').value;
+            Ajax(inputEmail);
+            formSuccess.style.display = "inline-block";
+        });
+
+        email.addEventListener('blur', (e)=>{
+            let val = e.target.value;
+            if(val != "") {
+                e.target.classList.add('has-value');
+            } else {
+                e.target.classList.remove('has-value');
+            }
+        });
+    }
+    );
+
+
 
 }
 
